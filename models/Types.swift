2391e1e4096d49ff18c11cadbfe93c8d8489b684
Ganza Iris <ganzairis03@gmail.com>
2025-11-10 17:12:08 +0200

    added variables needed in project implementation

--- models/Types.swift
+++ models/Types.swift
@@ -1,7 +1,61 @@
-//
-//  Types.swift
-//  Nourishing_life_with_us
-//
-//  Created by Urukundo Monday on 10/11/2025.
-//
+import Foundation
 
+struct User {
+    let id: String
+    let email: String
+    let name: String
+    let role: UserRole
+    let phone: String?
+    let profileImage: String?
+    let createdAt: Date
+}
+
+enum UserRole: String, Codable {
+    case admin, user
+}
+
+struct Disease: Identifiable, Codable {
+    let id: String
+    let name: String
+    let description: String
+    let icon: String
+    let color: String
+    let didYouKnow: [String]
+}
+
+struct Meal: Identifiable, Codable {
+    let id: String
+    let diseaseId: String
+    let category: MealCategory
+    let name: String
+    let description: String
+    let image: String
+    let preparationSteps: [String]
+    let nutrients: Nutrients
+    let benefits: [String]
+}
+
+struct Nutrients: Codable {
+    let calories: String
+    let protein: String
+    let carbs: String
+    let fats: String
+}
+
+enum MealCategory: String, Codable, CaseIterable {
+    case breakfast, lunch, dinner, snacks, drinks, vitamins
+}
+
+struct FAQ: Identifiable, Codable {
+    let id: String
+    let question: String
+    let answer: String
+}
+
+struct Rating: Identifiable, Codable {
+    let id: String
+    let userId: String
+    let rating: Int
+    let message: String
+    let createdAt: Date
+}
