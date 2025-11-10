e938612b76122f323b3462a7804ef8a44672a614
Ganza Iris <ganzairis03@gmail.com>
2025-11-10 16:53:54 +0200

    initialised swiftUI Project

---
+++ Nourishing_life_with_us/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nourishing_life_with_us
+//
+//  Created by Urukundo Monday on 10/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nourishing_life_with_usTests/Nourishing_life_with_usTests.swift
@@ -0,0 +1,16 @@
+//
+//  Nourishing_life_with_usTests.swift
+//  Nourishing_life_with_usTests
+//
+//  Created by Urukundo Monday on 10/11/2025.
+//
+
+import Testing
+
+struct Nourishing_life_with_usTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
